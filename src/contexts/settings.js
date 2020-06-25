a75435bd44bb28418e1eb18de748bcd69c482e20
cnickels21 <cnickels_21@yahoo.com>
2020-06-24 21:10:40 -0500

    settings context file defined

---
+++ src/contexts/settings.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+
+export const SettingsContext = React.createContext();
+
+export default function useSettings() {
+    return useContext(SettingsContext);
+}
+
+export class SettingsProvider extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            // TODO: figure out what state is
+        };
+    }
+
+    // TODO: Set state on properties once defined
+
+    render() {
+        return (
+            <SettingsContext.Provider value={this.state}>
+                {this.props.children}
+            </SettingsContext.Provider>
+        )
+    }
+}
