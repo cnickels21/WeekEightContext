72e7072e12ca10d9966471646be67625a0ceab86
cnickels21 <cnickels_21@yahoo.com>
2020-06-25 21:04:41 -0500

    auth context structured out and login method defined

---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header(props) {
+
+    return(
+        <header>
+            <h1>To Do or Not To Do!</h1>
+        </header>
+    );
+}
---
+++ src/contexts/auth.js
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import jwt, { JsonWebTokenError } from 'jsonwebtoken';
+
+const usersAPI = 'https://deltav-todo-alpha.azurewebsites.net/api/v1/Users';
+
+export const AuthContext = React.createContext();
+
+export default function useAuth() {
+  return useContext(AuthContext);
+}
+
+export default function AuthProvider(props) {
+
+    const login = async(username, password) => {
+        const result = await fetch(`${usersAPI}/Login`, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify([username, password]),
+        });
+
+        const body = await result.json();
+
+        if (result.ok) {
+            this.setState({ user: body });
+        };
+
+        this.setState({ user: null });
+    }
+
+    return (
+        <AuthContext.Provider value={this.state}>
+          {this.props.children}
+        </AuthContext.Provider>
+      );
+}
